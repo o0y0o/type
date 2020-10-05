e9acd1d1c3762d97186a61c5afc1df22a6b97c9c
jason.chung <shiningjason1989@gmail.com>
2020-10-05 22:50:34 +0800

    add nil constraint tests

---
+++ test/nil.test.js
@@ -0,0 +1,21 @@
+import t from '@lib'
+
+describe('type.nil', () => {
+  it('pass with undefined value', () => {
+    const actual = t.nil.validate(void 0)
+    expect(actual).toEqual({ valid: true })
+  })
+
+  it('pass with null value', () => {
+    const actual = t.nil.validate(null)
+    expect(actual).toEqual({ valid: true })
+  })
+
+  it('fail with non-nil value', () => {
+    const actual = t.nil.validate({})
+    expect(actual).toEqual({
+      valid: false,
+      errors: [{ expected: 'nil', actual: '{}' }]
+    })
+  })
+})
