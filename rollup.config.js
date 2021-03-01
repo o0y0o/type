2348aa5c4136836d954f15821476d7afeaf20f14
jason.chung <shiningjason1989@gmail.com>
2021-03-01 11:36:34 +0800

    upgrade packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const external = [pkg.dependencies, pkg.peerDependencies]
@@ -8,39 +7,19 @@ const external = [pkg.dependencies, pkg.peerDependencies]
   .flatMap(dep => Object.keys(dep))
   .map(pkg => new RegExp(`^${pkg.replace(/\//g, '/')}`))
 
-export default [
-  {
-    input: path.join(__dirname, pkg.src),
-    external,
-    plugins: [babel({ exclude: 'node_modules/**', babelHelpers: 'runtime' })],
-    output: [
-      {
-        file: path.join(__dirname, pkg.main),
-        format: 'cjs',
-        exports: 'default'
-      },
-      {
-        file: path.join(__dirname, pkg.module),
-        format: 'es'
-      }
-    ]
-  },
-  {
-    input: path.join(__dirname, pkg.src),
-    external,
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        babelHelpers: 'bundled',
-        plugins: [['@babel/transform-runtime', false]]
-      })
-    ],
-    output: {
-      file: path.join(__dirname, pkg.iife),
-      format: 'iife',
-      name: 'type',
-      plugins: [terser()],
-      globals: { lodash: '_' }
+export default {
+  input: path.join(__dirname, pkg.src),
+  external,
+  plugins: [babel({ exclude: 'node_modules/**', babelHelpers: 'runtime' })],
+  output: [
+    {
+      file: path.join(__dirname, pkg.main),
+      format: 'cjs',
+      exports: 'default'
+    },
+    {
+      file: path.join(__dirname, pkg.module),
+      format: 'es'
     }
-  }
-]
+  ]
+}
